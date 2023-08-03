125d843e50cfb3d2c471e8f1d507bea1cd2d56e2
VenuLgs <venu.koratla@labyrinthglobalsolutions.com>
2023-08-03 12:01:33 +0530

    nav links added to mobile view

--- src/Components/Popup/index.js
+++ src/Components/Popup/index.js
@@ -1,17 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import './index.css';
-import {RxCross2} from 'react-icons/rx'
+import React, { useState, useEffect } from "react";
+import "./index.css";
+import { RxCross2 } from "react-icons/rx";
 
-const FirstTimePopup = () => {
+const Contactpopup = () => {
   const [showPopup, setShowPopup] = useState(true);
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [course, setCourse] = useState('');
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
   const [phoneNumber, setPhoneNumber] = useState(null);
-  const [comment, setComment] = useState('');
-  const [successMsg,setSuccessMsg]=useState("");
-  const [errorMsg,setErrorMsg]=useState("")
-
+  const [comment, setComment] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
   const closePopup = () => {
     setShowPopup(false);
@@ -20,23 +19,20 @@ const FirstTimePopup = () => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
-      const response = await fetch(
-        "http://localhost:3005/register",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            name,
-            email,
-            course,
-            phoneNumber,
-            comment
-          })
-        }
-      );
-  
+      const response = await fetch("http://localhost:3005/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          course,
+          phoneNumber,
+          comment,
+        }),
+      });
+
       if (response.ok) {
         const data = await response.json();
         setSuccessMsg(data.success);
@@ -51,111 +47,107 @@ const FirstTimePopup = () => {
       console.error(error);
     }
   };
-  
 
   return (
     <>
       {showPopup && (
         <div className="popup">
           <div className="card-container-3">
-          
-          <div className="popup-image-container ">
-            <div>
-              <h1 className="popup-heading">Get a Free Demo</h1>
-              <p className="popup-para">
-                Please Fill the form our expert will contact you
-              </p>
-              <img
-                className="login-logo"
-                src="https://res.cloudinary.com/dg81jw9qd/image/upload/v1690546385/10-work_2x_fazttu.png"
-                alt="website login"
-              />
+            <div className="popup-image-container ">
+              <div>
+                <h1 className="popup-heading">Get a Free Demo</h1>
+                <p className="popup-para">
+                  Please Fill the form our expert will contact you
+                </p>
+                <img
+                  className="login-logo"
+                  src="https://res.cloudinary.com/dg81jw9qd/image/upload/v1690546385/10-work_2x_fazttu.png"
+                  alt="website login"
+                />
+              </div>
             </div>
+            <form onSubmit={handleSubmit} className="form-container">
+              <div className="close-container" onClick={closePopup}>
+                <RxCross2 className="close-1" />
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  id="name"
+                  placeholder="Enter your name"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="input-field"
+                  required
+                />
+              </div>
+              <div className="input-container">
+                <select
+                  id="course"
+                  value={course}
+                  onChange={(e) => setCourse(e.target.value)}
+                  className="input-field"
+                  required
+                >
+                  <option value="Mendix">Mendix</option>
+
+                  <option value="Polarian">Polarian</option>
+
+                  <option value="Salesforce">Salesforce</option>
+
+                  <option value="Mobile And Web Application">
+                    Mobile and Web Application
+                  </option>
+
+                  <option value="Saas And Cloud Solutions">
+                    SAAS And Cloud Solutions
+                  </option>
+                </select>
+              </div>
+              <div className="input-container">
+                <input
+                  type="number"
+                  id="phoneNumber"
+                  placeholder="Enter your phone number"
+                  value={phoneNumber}
+                  onChange={(e) => setPhoneNumber(e.target.value)}
+                  className="input-field"
+                  required
+                />
+              </div>
+              <div className="input-container">
+                {" "}
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Enter your email address"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="input-field"
+                  required
+                />
+              </div>
+              <div className="input-container">
+                <input
+                  type="text"
+                  id="comment"
+                  placeholder="Enter your Comment"
+                  value={comment}
+                  onChange={(e) => setComment(e.target.value)}
+                  className="input-field"
+                />
+              </div>
+              <div className="popup-button-container">
+                <button className="login-button" type="submit">
+                  SUBMIT
+                </button>
+              </div>
+            </form>
           </div>
-          <form onSubmit={handleSubmit} className="form-container">
-            <div className='close-container' onClick={closePopup}>
-              <RxCross2 className='close-1'  />
-            </div>
-            <div className="input-container">
-              <input
-                type="text"
-                id="name"
-                placeholder="Enter your name"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                className="input-field"
-                required
-              />
-            </div>
-            <div className="input-container">
-              <select
-                id="course"
-                value={course}
-                onChange={(e) => setCourse(e.target.value)}
-                className="input-field"
-                required
-              >
-                <option value="Mendix">Mendix</option>
-
-                <option value="Polarian">Polarian</option>
-
-                <option value="Salesforce">Salesforce</option>
-
-                <option value="Mobile And Web Application">
-                  Mobile and Web Application
-                </option>
-
-                <option value="Saas And Cloud Solutions">
-                  SAAS And Cloud Solutions
-                </option>
-              </select>
-            </div>
-            <div className="input-container">
-              <input
-                type="number"
-                id="phoneNumber"
-                placeholder="Enter your phone number"
-                value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
-                className="input-field"
-                required
-              />
-            </div>
-            <div className="input-container">
-              {" "}
-              <input
-                type="email"
-                id="email"
-                placeholder="Enter your email address"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="input-field"
-                required
-              />
-            </div>
-            <div className="input-container">
-              <input
-                type="text"
-                id="comment"
-                placeholder="Enter your Comment"
-                value={comment}
-                onChange={(e) => setComment(e.target.value)}
-                className="input-field"
-              />
-            </div>
-            <div className="popup-button-container">
-              <button className="login-button" type="submit">
-                SUBMIT
-              </button>
-            </div>
-           
-          </form>
-        </div>
         </div>
       )}
     </>
   );
 };
 
-export default FirstTimePopup;
-
+export default Contactpopup;
