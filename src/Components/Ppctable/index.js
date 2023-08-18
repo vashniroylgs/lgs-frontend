3fe532b3a3bb397022d0c2d697b9e9096cf360bf
VenuLgs <venu.koratla@labyrinthglobalsolutions.com>
2023-08-18 18:37:45 +0530

    commited

--- src/Components/Ppctable/index.js
+++ src/Components/Ppctable/index.js
@@ -1,156 +1,138 @@
-import React from 'react';
+import React from "react";
 
 const Data = [
   {
-    "Features": "Website Updates (Text, Photos, Pages, Navigation, Videos, Links)",
-    "BRONZE": "Maximum 5 Updates",
-    "SILVER": "Maximum 10 Updates",
-    "GOLD": "Maximum 20 Updates",
-    "PLATINUM": "Maximum 40 Updates"
-    
+    Features:
+      "Website Updates (Text, Photos, Pages, Navigation, Videos, Links)",
+    BRONZE: "Maximum 5 Updates",
+    SILVER: "Maximum 10 Updates",
+    GOLD: "Maximum 20 Updates",
+    PLATINUM: "Maximum 40 Updates",
   },
   {
-    "Features": "Technical Support Duration",
-    "BRONZE": "5 Hours Per Month",
-    "SILVER": "5 Hours Per Month",
-    "GOLD": "8 Hours Per Month",
-    "PLATINUM": "10 Hours Per Month"
-    
+    Features: "Technical Support Duration",
+    BRONZE: "5 Hours Per Month",
+    SILVER: "5 Hours Per Month",
+    GOLD: "8 Hours Per Month",
+    PLATINUM: "10 Hours Per Month",
   },
   {
-    "Features": "Work Completion Time",
-    "BRONZE": "3 Business Days",
-    "SILVER": "3 Business Days",
-    "GOLD": "2 Business Days",
-    "PLATINUM": "1 Business Days"
-    
+    Features: "Work Completion Time",
+    BRONZE: "3 Business Days",
+    SILVER: "3 Business Days",
+    GOLD: "2 Business Days",
+    PLATINUM: "1 Business Days",
   },
   {
-    "Features": "New Product Upload",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Extra Charges"
-    
+    Features: "New Product Upload",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Extra Charges",
   },
   {
-    "Features": "Domain & Hosting Assistance",
-    "BRONZE": "No",
-    "SILVER": "No",
-    "GOLD": "No",
-    "PLATINUM": "Yes"
-    
+    Features: "Domain & Hosting Assistance",
+    BRONZE: "No",
+    SILVER: "No",
+    GOLD: "No",
+    PLATINUM: "Yes",
   },
   {
-    "Features": "Website Customization Works",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Only Basic Changes Allowed",
-    "PLATINUM": "Only Basic Changes Allowed"
-    
+    Features: "Website Customization Works",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Only Basic Changes Allowed",
+    PLATINUM: "Only Basic Changes Allowed",
   },
   {
-    "Features": "Website Health Check",
-    "BRONZE": "Monthly",
-    "SILVER": "Monthly",
-    "GOLD": "Monthly",
-    "PLATINUM": "Weekly"
-    
+    Features: "Website Health Check",
+    BRONZE: "Monthly",
+    SILVER: "Monthly",
+    GOLD: "Monthly",
+    PLATINUM: "Weekly",
   },
   {
-    "Features": "Image Optimization",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Extra Charges"
-    
+    Features: "Image Optimization",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Extra Charges",
   },
   {
-    "Features": "Code Optimization",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Extra Charges"
-    
+    Features: "Code Optimization",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Extra Charges",
   },
   {
-    "Features": "WordPress Core & Plugin Update",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Only Basic Changes Will Be Done"
-    
+    Features: "WordPress Core & Plugin Update",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Only Basic Changes Will Be Done",
   },
   {
-    "Features": "Theme Updates*",
-    "BRONZE": "No",
-    "SILVER": "No",
-    "GOLD": "No",
-    "PLATINUM": "No"
-    
+    Features: "Theme Updates*",
+    BRONZE: "No",
+    SILVER: "No",
+    GOLD: "No",
+    PLATINUM: "No",
   },
   {
-    "Features": "Website Speed Optimization",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Extra Charges"
-    
+    Features: "Website Speed Optimization",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Extra Charges",
   },
   {
-    "Features": "Adding and Updating New Content",
-    "BRONZE": "Up to 1 Page",
-    "SILVER": "Up to 2 Page",
-    "GOLD": "Up to 3 Page",
-    "PLATINUM": "Up to 5 Page"
-    
+    Features: "Adding and Updating New Content",
+    BRONZE: "Up to 1 Page",
+    SILVER: "Up to 2 Page",
+    GOLD: "Up to 3 Page",
+    PLATINUM: "Up to 5 Page",
   },
   {
-    "Features": "Changes in Address Or Contact Information",
-    "BRONZE": "Yes",
-    "SILVER": "Yes",
-    "GOLD": "Yes",
-    "PLATINUM": "Yes"
-    
+    Features: "Changes in Address Or Contact Information",
+    BRONZE: "Yes",
+    SILVER: "Yes",
+    GOLD: "Yes",
+    PLATINUM: "Yes",
   },
   {
-    "Features": "Trouble Shooting Server Issues",
-    "BRONZE": "No",
-    "SILVER": "NO",
-    "GOLD": "No",
-    "PLATINUM": "Yes"
-    
+    Features: "Trouble Shooting Server Issues",
+    BRONZE: "No",
+    SILVER: "NO",
+    GOLD: "No",
+    PLATINUM: "Yes",
   },
   {
-    "Features": "Scan Website For Malicious Issues",
-    "BRONZE": "No",
-    "SILVER": "NO",
-    "GOLD": "No",
-    "PLATINUM": "Yes"
-    
+    Features: "Scan Website For Malicious Issues",
+    BRONZE: "No",
+    SILVER: "NO",
+    GOLD: "No",
+    PLATINUM: "Yes",
   },
   {
-    "Features": "Broken Link Check & Fix",
-    "BRONZE": "Extra Charges",
-    "SILVER": "Extra Charges",
-    "GOLD": "Extra Charges",
-    "PLATINUM": "Extra Charges"
-    
+    Features: "Broken Link Check & Fix",
+    BRONZE: "Extra Charges",
+    SILVER: "Extra Charges",
+    GOLD: "Extra Charges",
+    PLATINUM: "Extra Charges",
   },
   {
-    "Features": "Get Support On",
-    "BRONZE": "Email",
-    "SILVER": "Email",
-    "GOLD": "Phone/Email",
-    "PLATINUM": "Phone/Email"
-    
+    Features: "Get Support On",
+    BRONZE: "Email",
+    SILVER: "Email",
+    GOLD: "Phone/Email",
+    PLATINUM: "Phone/Email",
   },
-  
 ];
 
 const PpcTable = () => {
   return (
-    <table className='table-container'>
+    <table className="table-container">
       <thead>
         <tr>
           <th className="feature-cell">BASIC</th>
@@ -158,10 +140,10 @@ const PpcTable = () => {
           <th className="feature-cell">ENTERPRICE</th>
         </tr>
       </thead>
-      <tbody >
+      <tbody>
         {Data.map((row, index) => (
           <tr key={index}>
-            <td className='row-cell'>{row.Features}</td>
+            <td className="row-cell">{row.Features}</td>
             <td>{row.BRONZE}</td>
             <td>{row.SILVER}</td>
             <td>{row.GOLD}</td>
